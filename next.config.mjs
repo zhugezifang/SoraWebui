f0cc7bc19661323d00448798649e7b77a1687859
诸葛子房 <xiaoqiu2017wy@163.com>
2025-07-20 20:54:31 +0800

    feat:next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-// next.config.js
-module.exports = {
-    experimental: {
-      runtime: 'experimental-edge', // 启用 Edge Runtime
-      serverComponents: true, // 如果使用 Server Components
-    },
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
